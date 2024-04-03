9ec1b683c60f4f56549f376df90d0105e279ad88
Victor Correal <victor.correal@bsc.es>
2024-04-04 00:26:22 +0200

    remove utils

--- C_PLUSPLUS_APPS/UTILS/include/rand_init.hh
+++
@@ -1,8 +0,0 @@
-#ifndef __RAND_INIT__
-#define __RAND_INIT__
-
-#include <cstdlib>      // random
-
-void fill_array(int data[], int N, int min, int max, int seed);
-
-#endif
--- C_PLUSPLUS_APPS/UTILS/include/random_init.h
+++
@@ -1,4 +0,0 @@
-
-#include <stdlib.h>
-
-void fill_array(int data[], int N, int min, int max, int seed);
