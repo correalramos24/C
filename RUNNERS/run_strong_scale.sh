6af50bd8ce8cd4d5c77500327d474a39abebe41e
Victor Correal <victor.correal@bsc.es>
2024-04-06 03:23:12 +0200

    WIP #4

---
+++ RUNNERS/run_strong_scale.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# All the binaries have the same strucute for the inputs: 
+# bin <size>
+
+THREADS="1 2 4 8 16"
+BIN=$1
+SIZE=$2
+
+for T in $THREADS
+do
+    
+    echo -n "Running with $T ..."
+
+    # Parse results
+
+done
+
+# Store results
---
+++ RUNNERS/run_weak_scale.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# All the binaries have the same strucute for the inputs: 
+# bin <size>
+
+BIN=$1
+INIT_SIZE=$2
+THREADS=$3
+
